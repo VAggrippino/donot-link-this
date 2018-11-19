3fa42e20110c58c803c8830d37c562e3d17d926a
Vince Aggrippino <vinceaggrippino@gmail.com>
2018-11-19 20:09:15 +0800

    Implement a fix button to demonstrate the fix

--- src/index.js
+++ src/index.js
@@ -22,3 +22,29 @@ const exampleInput = document.getElementsByClassName('example__input')[0];
 const exampleOutput = document.getElementsByClassName('example__output')[0];
 
 exampleOutput.innerHTML = linkifyHtml(exampleInput.value, linkifyOptions);
+
+exampleInput.addEventListener('change', (e) => {
+  exampleOutput.innerHTML = linkifyHtml(exampleInput.value, linkifyOptions);
+});
+
+exampleInput.addEventListener('keyup', (e) => {
+  exampleOutput.innerHTML = linkifyHtml(exampleInput.value, linkifyOptions);
+});
+
+exampleInput.addEventListener('mousedown', (e) => {
+  exampleOutput.innerHTML = linkifyHtml(exampleInput.value, linkifyOptions);
+});
+
+exampleInput.addEventListener('mouseup', (e) => {
+  exampleOutput.innerHTML = linkifyHtml(exampleInput.value, linkifyOptions);
+});
+
+const fixButton = document.getElementsByClassName('example__fix')[0];
+fixButton.addEventListener('click', (e) => {
+  console.log('fix');
+  let msg = exampleInput.value;
+  msg = msg.replace(/([@#\.])/g, '$1\u200B');
+  msg = msg.replace(/(https?)(:\/\/)/g, '$1$2\u200B');
+  exampleInput.value = msg;
+  exampleOutput.innerHTML = linkifyHtml(exampleInput.value, linkifyOptions);
+});
