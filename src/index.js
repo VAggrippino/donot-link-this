d199da46af93215239e7fe6213748cbbdf57d97f
Vince Aggrippino <vinceaggrippino@gmail.com>
2018-11-19 19:40:44 +0800

    Use linkifyjs to demonstrate the problem and update the demo

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import * as linkify from 'linkifyjs';
+import linkifyHtml from 'linkifyjs/html';
+import hashtag from 'linkifyjs/plugins/hashtag';
+import mention from 'linkifyjs/plugins/mention';
+import ticket from 'linkifyjs/plugins/ticket';
+
+hashtag(linkify);
+mention(linkify);
+ticket(linkify);
+
+const linkifyOptions = {
+  defaultProtocol: 'https',
+  formatHref: {
+    url: (target) => target,
+    hashtag: (target) => 'https://twitter.com/hashtag/' + target.substring(1),
+    mention: (target) => 'https://twitter.com/' + target.substring(1),
+    ticket: (target) => 'https://github.com/VAggrippino/donot-link-this/issues/' + target.substring(1),
+  },
+};
+
+const exampleInput = document.getElementsByClassName('example__input')[0];
+const exampleOutput = document.getElementsByClassName('example__output')[0];
+
+exampleOutput.innerHTML = linkifyHtml(exampleInput.value, linkifyOptions);
