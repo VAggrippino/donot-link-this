97f782f7c0ea4cb96c0cc6e0a5c977ae980fc54f
Vince Aggrippino <vinceaggrippino@gmail.com>
2018-12-01 20:40:28 +0800

    Add a UI to the bookmarklet to allow manual copy/paste

--- src/js/bookmarklet.js
+++ src/js/bookmarklet.js
@@ -7,25 +7,46 @@
         .replace(/([@#\.])/g, '$1\u200B')
         .replace(/(https?)(:\/\/)/g, '$1\u200B$2');
 
-    // Determine what's selected
-    let target = null;
-    // If the anchorNode and focusNode are the same AND the anchorOffset and
-    // focusOffset are the same, the selection must be inside a form field.
-    if (selection.anchorNode === selection.focusNode &&
-      selection.anchorOffset === selection.focusOffset
-    // It's a form field
-    ) {
-      target = selection.anchorNode.childNodes[selection.anchorOffset];
-      const value = target.value;
-      const before = value.substring(0, target.selectionStart);
-      const after = value.substring(target.selectionEnd + 1);
-      target.value = before + replacement + after;
+    // Create the user interface
+    const uiDiv = document.createElement('div');
+    const uiTextarea = document.createElement('textarea');
+    const uiCopyButton = document.createElement('button');
+    const uiCloseButton = document.createElement('button');
 
-    // It's a block of text
-    } else {
-      target = selection.getRangeAt(0);
-      target.deleteContents();
-      target.insertNode(document.createTextNode(replacement));
-    }
+    // Style the UI container
+    uiDiv.style.position = 'absolute';
+    uiDiv.style.top = '50%';
+    uiDiv.style.left = '50%';
+    uiDiv.style.transform = 'translate(-50%,-50%)';
+    uiDiv.style.zIndex = '9001';
+    uiDiv.style.padding = '1em';
+    uiDiv.style.backgroundColor = 'bisque';
+    uiDiv.style.borderRadius = '0.5em';
+    uiDiv.style.boxShadow = '2px 2px 10px rgba(0,0,0,0.3)';
+
+    // Style the UI textarea
+    uiTextarea.setAttribute('rows', '4');
+    uiTextarea.style.width = '33vw';
+    uiTextarea.value = replacement;
+
+    // Style the UI copy button and handle the click event
+    uiCopyButton.appendChild(document.createTextNode('Copy'));
+    uiCopyButton.addEventListener('click', (e) => {
+      e.target.parentNode.getElementsByTagName('textarea')[0].select();
+      document.execCommand('copy');
+    });
+
+    // Style the close button and handle the click event
+    uiCloseButton.appendChild(document.createTextNode('Close'));
+    uiCloseButton.addEventListener('click', () => {
+      document.body.removeChild(uiDiv);
+    });
+
+    // Assemble the pieces
+    uiDiv.appendChild(uiTextarea);
+    uiDiv.appendChild(document.createElement('br'));
+    uiDiv.appendChild(uiCopyButton);
+    uiDiv.appendChild(uiCloseButton);
+    document.body.appendChild(uiDiv);
   }
 })();
